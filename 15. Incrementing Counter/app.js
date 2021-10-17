4f895a1516365db4a6e68f8fe0b6089e7c7cc12b
MoonK <ansrud45@gmail.com>
2021-10-17 13:06:56 +0900

    Incrementing Counter: Update app.js

--- 15. Incrementing Counter/app.js
+++ 15. Incrementing Counter/app.js
@@ -3,11 +3,10 @@ const counters = document.querySelectorAll('.counter');
 counters.forEach((counter) => {
   counter.innerText = '0';
 
+  const target = +counter.getAttribute('data-target');
+  const increment = target / 200;
   const updateCounter = () => {
-    const target = +counter.getAttribute('data-target');
     const c = +counter.innerText;
-
-    const increment = target / 200;
 
     if (c < target) {
       console.log(`c: ${c}, increment: ${increment}`);
